158bc7202f06e16659cafbac54b273b23972d5a7
Haneet Singh <singh.haneet007@gmail.com>
2026-08-22 13:30:52 -0400

    Assemble homepage from sections

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
+import { Services } from "@/components/Services";
+import { Approach } from "@/components/Approach";
+import { Contact } from "@/components/Contact";
+import { Footer } from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="flex-1">
+        <Hero />
+        <Services />
+        <Approach />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+}
